6d553a9588eb63c6465b896cc7563d467c95978a
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-08-09 22:57:49 -0500

    Adjust layout by adding containers and using grid

--- src/Post.jsx
+++ src/Post.jsx
@@ -4,11 +4,18 @@ import './Post.css'
 function Post({ id, content, isPinned, deletePost, togglePin }) {
 
   return (
-    <li className={`post ${isPinned ? "pinned" : ""}`} onDoubleClick={() => togglePin(id, isPinned)}>
-      <span>{content}</span>
-      <button className="btn--delete" onClick={() => deletePost(id)}>Delete</button>
-      {/* <button className="btn--pin" onClick={() => togglePin(id, isPinned)}>{isPinned ? "Unpin" : "Pin"}</button> */}
-    </li>
+    <div className="post-outer-box">
+      <li className={`post ${isPinned ? "pinned" : ""}`} onDoubleClick={() => togglePin(id, isPinned)}>
+        <div className="post-content">
+          <img className="user-icon" src="https://a.slack-edge.com/80588/marketing/img/avatars/slackbot/avatar-slackbot@2x.png" alt="Slackbot Icon" />
+          <p className="user-name">You</p>
+          <p className="post-text">{content}</p>
+        </div>
+        <button className="btn--delete" onClick={() => deletePost(id)}>Delete</button>
+        {/* <button className="btn--pin" onClick={() => togglePin(id, isPinned)}>{isPinned ? "Unpin" : "Pin"}</button> */}
+      </li>
+    </div>
+    
   )
 }
 
