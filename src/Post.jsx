28a5ffb5cda8d7b2fb54c36906271bbcc62732bc
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-08-09 20:07:07 -0500

    Implement double-click pinning

--- src/Post.jsx
+++ src/Post.jsx
@@ -4,10 +4,10 @@ import './Post.css'
 function Post({ id, content, isPinned, deletePost, togglePin }) {
 
   return (
-    <li className={`post ${isPinned ? "pinned" : ""}`}>
+    <li className={`post ${isPinned ? "pinned" : ""}`} onDoubleClick={() => togglePin(id, isPinned)}>
       <span>{content}</span>
       <button className="btn--delete" onClick={() => deletePost(id)}>Delete</button>
-      <button className="btn--pin" onClick={() => togglePin(id, isPinned)}>{isPinned ? "Unpin" : "Pin"}</button>
+      {/* <button className="btn--pin" onClick={() => togglePin(id, isPinned)}>{isPinned ? "Unpin" : "Pin"}</button> */}
     </li>
   )
 }
